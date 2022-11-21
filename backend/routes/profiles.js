74683edd76472006c7c50f1245a7bc52eb0a98e8
Lavie Levi <lavie.levi@gmail.com>
2022-11-20 23:43:12 -0800

    Add 'update' and 'delete' option to profiles route

--- backend/routes/profiles.js
+++ backend/routes/profiles.js
@@ -11,11 +11,37 @@ router.route('/add').post((req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const preferences = req.body.preferences;
-const newProfile = new Profile({username, email, preferences});
+    const newProfile = new Profile({username, email, preferences});
 
     newProfile.save()
         .then(() => res.json('Profile added!'))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req,res) => {
+    Profile.findById(req.params.id)
+        .then(profile => res.json(profile))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req,res) => {
+    Profile.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Profile deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+}); 
+
+router.route('/update/:id').post((req, res) => {
+    Profile.findById(req.params.id)
+      .then(profile => {
+        profile.username = req.body.username;
+        profile.email = req.body.email;
+        profile.preferences = req.body.preferences;
+
+        profile.save()
+          .then(() => res.json('Profile updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
 module.exports = router;
