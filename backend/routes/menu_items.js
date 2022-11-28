458b08dc9448a3c6e93418f2ab7f2bf2c06b16c4
akivabrookler <61440854+akivabrookler@users.noreply.github.com>
2022-11-27 22:52:39 -0800

    Added Rating Feature and fixed npm packaging
    Added a dislike and like feature. The feature is currently available to
    all users, with no the limit to the number of likes and dislikes only
    per visit to page.
    TODO: Limit the feature to when the user is logged in and limit the
    quantity to one per user.
    
    The MenuItem page currently displays the information, but not in a
    particularly aesthetically pleasing way.
    TODO: Update the aesthetics
    
    Fixed npm packaging on branch that was a result of mistaken commits
    that pushed a node_modules folder and unneccsary packages. Also,
    changed a .env file that was mistakenly pushed to main.

--- backend/routes/menu_items.js
+++ backend/routes/menu_items.js
@@ -13,4 +13,48 @@ router.route('/:id').get((req,res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/likeinc/:id').post((req, res) => {
+    Menu_Item.findById(req.params.id)
+      .then(menuItem => {
+        menuItem.likes += 1;
+        menuItem.save()
+            .then(() => res.json('Like incremented!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  router.route('/likedec/:id').post((req, res) => {
+    Menu_Item.findById(req.params.id)
+      .then(menuItem => {
+        menuItem.likes -= 1;
+        menuItem.save()
+            .then(() => res.json('Like decremented!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  router.route('/dislikeinc/:id').post((req, res) => {
+    Menu_Item.findById(req.params.id)
+      .then(menuItem => {
+        menuItem.dislikes += 1;
+        menuItem.save()
+            .then(() => res.json('Dislike incremented!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+  router.route('/dislikedec/:id').post((req, res) => {
+    Menu_Item.findById(req.params.id)
+      .then(menuItem => {
+        menuItem.dislikes -= 1;
+        menuItem.save()
+            .then(() => res.json('Dislike decremented!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+  });
+
 module.exports = router;
--- mynu/src/components/MenuItem.component.js
+++ mynu/src/components/MenuItem.component.js
@@ -1,43 +1,169 @@
 import { Component} from 'react';
 import axios from 'axios';
+import {ListItem, List, IconButton, ButtonGroup } from '@mui/material';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+
+
 
 export default class MenuItem extends Component {
 
     constructor(props) {
-        super(props);  
+        super(props);
         this.state = {
             id: '',
             name: '',
-            allergens: '',
+            allergens: [],
             dining_hall: '',
-            meal: []      
+            meal: [],
+            likes: 0,
+            dislikes: 0,
+            liked: false,
+            disliked: false      
         };
+        this.handleLike = this.handleLike.bind(this);
+        this.handleDisike = this.handleDislike.bind(this);
+
     }
 
     async componentDidMount() {
-    console.log("Called mount")
-    let id =(window.location.pathname).replace("/menu/","");
-    await axios.get('http://localhost:5000/menu_items/' + id)
-        .then(response => {
-        this.setState({
-            id: response.data.id,
-            name: response.data.name,
-            allergens: response.data.allergens,
-            dining_hall: response.data.allergens,
-            meal: response.data.meal
+        console.log("Called mount")
+        let id =(window.location.pathname).replace("/menu/","");
+        await axios.get('http://localhost:5000/menu_items/' + id)
+            .then(response => {
+            this.setState({
+                id: response.data._id,
+                name: response.data.name,
+                allergens: response.data.allergens,
+                dining_hall: response.data.dining_hall,
+                meal: response.data.meal,
+                likes: response.data.likes,
+                dislikes: response.data.dislikes
+                })
             })
-        })
-        .catch((error) => {
-        console.log(error);
-        })
+            .catch((error) => {
+            console.log(error);
+            })
+    }
+
+    async handleLike() { 
+        let liked = this.state.liked;
+        let likes = this.state.likes
+        let disliked = this.state.disliked;
+        let dislikes = this.state.dislikes
+
+        console.log(this.state)
+        let id =(window.location.pathname).replace("/menu/","");
+        if (this.state.liked){
+             await axios.post('http://localhost:5000/menu_items/likedec/' + id, {})
+              .then(function (response) {
+                console.log(response);
+                liked = false;
+                likes -= 1;
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }else{
+            await axios.post('http://localhost:5000/menu_items/likeinc/' + id, {})
+              .then(function (response) {
+                console.log(response);
+                liked = true;
+                likes += 1;
+
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+              if (this.state.disliked){
+                await axios.post('http://localhost:5000/menu_items/dislikedec/' + id, {})
+                .then(function (response) {
+                    console.log(response);
+                    disliked = false;
+                    dislikes -= 1;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            }
+        }
+        this.setState({liked: liked, likes: likes, disliked: disliked, dislikes: dislikes});
+    }
+
+    async handleDislike() { 
+        let disliked = this.state.disliked;
+        let dislikes = this.state.dislikes
+        let liked = this.state.liked;
+        let likes = this.state.likes
+
+        console.log(this.state)
+        let id =(window.location.pathname).replace("/menu/","");
+        if (this.state.disliked){
+             await axios.post('http://localhost:5000/menu_items/dislikedec/' + id, {})
+              .then(function (response) {
+                console.log(response);
+                disliked = false;
+                dislikes -= 1;
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }else{
+            await axios.post('http://localhost:5000/menu_items/dislikeinc/' + id, {})
+              .then(function (response) {
+                console.log(response);
+                disliked = true;
+                dislikes += 1;
+
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+              if (this.state.liked){
+                await axios.post('http://localhost:5000/menu_items/likedec/' + id, {})
+                .then(function (response) {
+                    console.log(response);
+                    liked = false;
+                    likes -= 1;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            }
+        }
+        this.setState({liked: liked, likes: likes, disliked: disliked, dislikes: dislikes});
     }
     
 
 
     render(){
+
+        let allergens = this.state.allergens;
+        let allergenList=[];
+        allergens.forEach((item, index) => {
+            allergenList.push( <ListItem key={index} class = "list-group-item">{item}</ListItem>)
+        })
+
+        let clickedLikeColoring = (this.state.liked) ? "primary" : "";
+        let clickedDislikeColoring = (this.state.disliked) ? "error" : "";
+
         return (
             <div>
-                <p>{this.state.name}</p>
+                <h1 class="text-center font-italic font-weight-light">{this.state.name}</h1>
+                <div class="d-flex align-items-left flex-column">
+                    <h1>Dining Hall: {this.state.dining_hall}</h1>
+                    <h1>Allergens</h1>
+                    <List >
+                        {allergenList}
+                    </List>
+                </div>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                    <IconButton onClick={()=>{this.handleLike()}} color ={clickedLikeColoring}> {this.state.likes}<ThumbUpIcon/> </IconButton>
+                    <IconButton onClick={()=>{this.handleDislike()}} color ={clickedDislikeColoring}> {this.state.dislikes}<ThumbDownIcon/> </IconButton>
+                </ButtonGroup>
+
+
+
                 
             </div>
         );
