8f174abd2c4dff3b5f44394d1c019b9921cbdda8
ugathoo <urjagathoo@ucla.edu>
2022-11-29 14:42:27 -0800

    modified:   mynu/src/components/MenuList.component.js

--- mynu/src/components/MenuList.component.js
+++ mynu/src/components/MenuList.component.js
@@ -1,5 +1,4 @@
-import Checkbox from '@mui/material/Checkbox';
-import {FormControlLabel, TextField, ListItem, ListItemButton, List} from '@mui/material';
+import {FormControlLabel, Checkbox, TextField, ListItem, ListItemButton, List, FormControl} from '@mui/material';
 import { Component} from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
@@ -90,30 +89,36 @@ export default class MenuList extends Component {
 
         return (
             <div class="container text-center">
-                <div class="row">
-                    <div class="col-sm">
-                        <h>Menu Items</h>
-                        <List class="list-group">
-                            {itemList}
-                        </List>
+                <div class="d-flex-row align-content-center p-3">
+                        <h1 class='shadow-lg bg-info text-light'>Menu Items</h1>
+                            <FormControl fullWidth>
+                                <FormControlLabel labelPlacement="top" control={<TextField label="Search Menu" />} onChange= {event => this.setState({search: (event.target.value).toLowerCase()}, this.handleFilter)}/>
+                            </FormControl>
+                            
+                </div>
+                <div class="d-flex flex-row">
+                    <div class="d-flex-column col-6">
+                            
+                            <List class="list-group">
+                                {itemList}
+                            </List>
                     </div>
-                    <div class="col-sm-2">
-                        <FormControlLabel control={<TextField label="Search Menu" />} onChange= {event => this.setState({search: (event.target.value).toLowerCase()}, this.handleFilter)}/>
-                        <div class="bg-light">
-                        <p>Dietary Restrictions</p>
-                        <List>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ vegetarian: e.target.checked}, this.handleFilter)}}/>} label="Vegetarian" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ eggFree: e.target.checked}, this.handleFilter)}}/>} label="Egg Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ vegan: e.target.checked}, this.handleFilter)}}/>} label="Vegan" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ glutenFree: e.target.checked}, this.handleFilter)}}/>} label="Gluten Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ treeNutFree: e.target.checked}, this.handleFilter)}}/>} label="Tree Nut Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ wheatFree: e.target.checked}, this.handleFilter)}}/>} label="Wheat Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ dairyFree: e.target.checked}, this.handleFilter)}}/>} label="Dairy Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ fishFree: e.target.checked}, this.handleFilter)}}/>} label="Fish Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ pescatarian: e.target.checked}, this.handleFilter)}}/>} label="Pescatarian" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ soyFree: e.target.checked}, this.handleFilter)}}/>} label="Soy Free" /></ListItem>
-                            <ListItem disablePadding><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ crustaceanShellfishFree: e.target.checked}, this.handleFilter)}}/>} label="Crustacean Shellfish Free"/></ListItem>   
-                        </List>
+                    <div class="d-flex-column col-6 p-3">
+                        <div class="p-3 d-flex-column bg-light">
+                            <h6 class = "text-center align-bottom font-weight-bold" >Dietary Restrictions</h6>
+                            <List>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ vegetarian: e.target.checked}, this.handleFilter)}}/>} label="Vegetarian" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ eggFree: e.target.checked}, this.handleFilter)}}/>} label="Egg Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ vegan: e.target.checked}, this.handleFilter)}}/>} label="Vegan" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ glutenFree: e.target.checked}, this.handleFilter)}}/>} label="Gluten Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ treeNutFree: e.target.checked}, this.handleFilter)}}/>} label="Tree Nut Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ wheatFree: e.target.checked}, this.handleFilter)}}/>} label="Wheat Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ dairyFree: e.target.checked}, this.handleFilter)}}/>} label="Dairy Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ fishFree: e.target.checked}, this.handleFilter)}}/>} label="Fish Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ pescatarian: e.target.checked}, this.handleFilter)}}/>} label="Pescatarian" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ soyFree: e.target.checked}, this.handleFilter)}}/>} label="Soy Free" /></ListItem>
+                                <ListItem ><FormControlLabel control={<Checkbox size="Medium" onChange={e=>{this.setState({ crustaceanShellfishFree: e.target.checked}, this.handleFilter)}}/>} label="Crustacean Shellfish Free"/></ListItem>   
+                            </List>
                         </div>
                     </div>
                 </div>
